9055f7c9dee09f9fed3d997359fef97c8df545a8
Gabriel Silveira Peruchi <gabriel-peruchi@hotmail.com>
2021-04-21 00:18:10 -0300

    Config typeorm e create entitie/controller Setting

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
-import express, { response } from "express";
+import express from "express"
 
-const app = express();
+import "./database"
+import { routes } from "./routes"
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hellow World!",
-  });
-});
+const app = express()
 
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso!",
-  });
-});
+app.use(express.json())
+app.use(routes)
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+app.listen(3333, () => console.log("Server is running on port 3333"))
