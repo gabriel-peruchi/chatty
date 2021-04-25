9ac76a9c4f7a7f7808029fe67c0587b1a084f236
Gabriel Silveira Peruchi <gabriel-peruchi@hotmail.com>
2021-04-25 11:01:48 -0300

    Include temporary frontend for testing

---
+++ public/js/admin.js
@@ -0,0 +1,93 @@
+const socket = io()
+let connectionsUsers = []
+
+socket.on("admin_list_all_users", connections => {
+    connectionsUsers = connections
+    document.getElementById("list_users").innerHTML = ""
+
+    const template = document.getElementById("template").innerHTML
+
+    connections.forEach(connection => {
+        const rendered = Mustache.render(template, {
+            id: connection.user_id,
+            email: connection.user.email
+        })
+
+        document.getElementById("list_users").innerHTML += rendered
+    })
+})
+
+function call(user_id) {
+    const connectionUser = connectionsUsers.find(connection => connection.user_id === user_id)
+
+    const template = document.getElementById("admin_template").innerHTML
+
+    const rendered = Mustache.render(template, {
+        id: connectionUser.user_id,
+        email: connectionUser.user.email
+    })
+
+    document.getElementById("supports").innerHTML += rendered
+
+    const params = {
+        user_id: connectionUser.user_id
+    }
+
+    socket.emit("admin_user_in_support", params)
+
+    socket.emit("admin_list_messages_by_user", params, messages => {
+        const divMessagesByUser = document.getElementById(`allMessages${connectionUser.user_id}`)
+
+        messages.forEach(message => {
+            const divMessage = document.createElement('div')
+
+            if (!message.admin_id) {
+                divMessage.className = "admin_message_client"
+                divMessage.innerHTML = `<span>${connectionUser.user.email}<span/>`
+                divMessage.innerHTML += `<span>${message.text}<span/>`
+                divMessage.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format('DD/MM/YYYY HH:mm:ss')}<span/>`
+            } else {
+                divMessage.className = "admin_message_admin"
+                divMessage.innerHTML = `Atendente: <span>${message.text}<span/>`
+                divMessage.innerHTML += `<span class="admin_date">${dayjs(message.created_at).format('DD/MM/YYYY HH:mm:ss')}<span/>`
+            }
+
+            divMessagesByUser.appendChild(divMessage)
+        })
+    })
+}
+
+function sendMessage(user_id) {
+    const text = document.getElementById(`send_message_${user_id}`)
+
+    const params = {
+        text: text.value,
+        user_id
+    }
+
+    socket.emit("admin_send_message", params)
+
+    const divMessages = document.getElementById(`allMessages${user_id}`)
+
+    const divMessage = document.createElement('div')
+    divMessage.className = "admin_message_admin"
+    divMessage.innerHTML = `Atendente: <span>${params.text}<span/>`
+    divMessage.innerHTML += `<span class="admin_date">${dayjs().format('DD/MM/YYYY HH:mm:ss')}<span/>`
+
+    divMessages.appendChild(divMessage)
+    text.value = ""
+}
+
+socket.on("admin_receive_message", data => {
+    const connectionUser = connectionsUsers.find(connection => connection.socket_id === data.socket_id)
+
+    const divMessage = document.createElement('div')
+
+    divMessage.className = "admin_message_client"
+    divMessage.innerHTML = `<span>${connectionUser.user.email}<span/>`
+    divMessage.innerHTML += `<span>${data.message.text}<span/>`
+    divMessage.innerHTML += `<span class="admin_date">${dayjs(data.message.created_at).format('DD/MM/YYYY HH:mm:ss')}<span/>`
+
+    const divMessagesByUser = document.getElementById(`allMessages${connectionUser.user_id}`)
+    divMessagesByUser.appendChild(divMessage)
+})
---
+++ public/js/chat.js
@@ -0,0 +1,78 @@
+let socket_admin_id = null
+let emailUser = null
+let socket = null
+
+document.querySelector("#start_chat").addEventListener("click", (event) => {
+    socket = io()
+
+    const chat_help = document.getElementById("chat_help")
+    chat_help.style.display = "none"
+
+    const chat_in_support = document.getElementById("chat_in_support")
+    chat_in_support.style.display = "block"
+
+    const email = document.getElementById("email").value
+    const text = document.getElementById("txt_help").value
+
+    emailUser = email
+
+    socket.on("connect", () => {
+        const params = { email, text }
+
+        socket.emit("client_first_access", params, (callback, error) => {
+            if (error) return console.log(error)
+            console.log(callback)
+        })
+    })
+
+    socket.on("client_list_all_messages", messages => {
+        const template_client = document.getElementById('message-user-template').innerHTML
+        const template_admin = document.getElementById('admin-template').innerHTML
+
+        messages.forEach(message => {
+            if (!message.admin_id) {
+                const rendered = Mustache.render(template_client, {
+                    message: message.text,
+                    email
+                })
+
+                document.getElementById("messages").innerHTML += rendered
+            } else {
+                const rendered = Mustache.render(template_admin, {
+                    message_admin: message.text
+                })
+
+                document.getElementById("messages").innerHTML += rendered
+            }
+        })
+    })
+
+    socket.on("admin_send_to_client", message => {
+        socket_admin_id = message.socket_id
+
+        const templateAdmin = document.getElementById("admin-template").innerHTML
+
+        const rendered = Mustache.render(templateAdmin, {
+            message_admin: message.text
+        })
+
+        document.getElementById("messages").innerHTML += rendered
+    })
+})
+
+document.getElementById('send_message_button').addEventListener('click', (event) => {
+    const text = document.getElementById("message_user").value
+
+    const params = {
+        text,
+        socket_admin_id
+    }
+
+    socket.emit("client_send_to_admin", params)
+
+    const templateClient = document.getElementById("message-user-template").innerHTML
+
+    const rendered = Mustache.render(templateClient, { message: text, email: emailUser })
+
+    document.getElementById("messages").innerHTML += rendered
+})
